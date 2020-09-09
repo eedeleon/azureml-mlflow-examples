d71279373ab1f32a6aeb6ac67489088b58ba6ae2
Eduardo de Leon <eddeleon@microsoft.com>
2020-09-09 19:01:21 -0400

    Use mlflow OSS keras example

--- train.py
+++ train.py
@@ -1,83 +1,58 @@
-# Copyright (c) Microsoft Corporation. All rights reserved.
-# Licensed under the MIT License.
-
-import mlflow
-import mlflow.keras
+"""Trains and evaluate a simple MLP
+on the Reuters newswire topic classification task.
+"""
 import numpy as np
-import warnings
-
 import keras
+from keras.datasets import reuters
 from keras.models import Sequential
-from keras.layers import Dense
-from keras.optimizers import RMSprop
+from keras.layers import Dense, Dropout, Activation
+from keras.preprocessing.text import Tokenizer
 
-import sys
+# The following import and function call are the only additions to code required
+# to automatically log metrics and parameters to MLflow.
+import mlflow.keras
 
-
-
-
-print("Keras version:", keras.__version__)
-
-# Enable auto-logging to MLflow to capture Keras metrics.
 mlflow.keras.autolog()
 
-# Model / data parameters
-n_inputs = 28 * 28
-n_h1 = 300
-n_h2 = 100
-n_outputs = 10
-learning_rate = 0.001
+max_words = 1000
+batch_size = 32
+epochs = 5
 
-# the data, split between train and test sets
-(x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
+print("Loading data...")
+(x_train, y_train), (x_test, y_test) = reuters.load_data(num_words=max_words, test_split=0.2)
 
-# Scale images to the [0, 1] range
-x_train = x_train.astype("float32") / 255
-x_test = x_test.astype("float32") / 255
+print(len(x_train), "train sequences")
+print(len(x_test), "test sequences")
 
-# Flatten image to be (n, 28 * 28)
-x_train = x_train.reshape(len(x_train), -1)
-x_test = x_test.reshape(len(x_test), -1)
+num_classes = np.max(y_train) + 1
+print(num_classes, "classes")
 
+print("Vectorizing sequence data...")
+tokenizer = Tokenizer(num_words=max_words)
+x_train = tokenizer.sequences_to_matrix(x_train, mode="binary")
+x_test = tokenizer.sequences_to_matrix(x_test, mode="binary")
 print("x_train shape:", x_train.shape)
-print(x_train.shape[0], "train samples")
-print(x_test.shape[0], "test samples")
+print("x_test shape:", x_test.shape)
 
-# convert class vectors to binary class matrices
-y_train = keras.utils.to_categorical(y_train, n_outputs)
-y_test = keras.utils.to_categorical(y_test, n_outputs)
+print("Convert class vector to binary class matrix " "(for use with categorical_crossentropy)")
+y_train = keras.utils.to_categorical(y_train, num_classes)
+y_test = keras.utils.to_categorical(y_test, num_classes)
+print("y_train shape:", y_train.shape)
+print("y_test shape:", y_test.shape)
 
+print("Building model...")
+model = Sequential()
+model.add(Dense(512, input_shape=(max_words,)))
+model.add(Activation("relu"))
+model.add(Dropout(0.5))
+model.add(Dense(num_classes))
+model.add(Activation("softmax"))
 
-def driver():
-    warnings.filterwarnings("ignore")
+model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
 
-    with mlflow.start_run() as run:
-
-        # Build a simple MLP model
-        model = Sequential()
-        # first hidden layer
-        model.add(Dense(n_h1, activation='relu', input_shape=(n_inputs,)))
-        # second hidden layer
-        model.add(Dense(n_h2, activation='relu'))
-        # output layer
-        model.add(Dense(n_outputs, activation='softmax'))
-        model.summary()
-
-        batch_size = 128
-        epochs = int(sys.argv[1]) if len(sys.argv) > 1 else 5
-
-        model.compile(loss='categorical_crossentropy',
-                      optimizer=RMSprop(lr=learning_rate),
-                      metrics=['accuracy'])
-
-        model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, validation_split=0.1)
-
-        score = model.evaluate(x_test, y_test, verbose=0)
-        print('Test loss:', score[0])
-        print('Test accuracy:', score[1])
-
-    return run
-
-
-if __name__ == "__main__":
-    driver()
+history = model.fit(
+    x_train, y_train, batch_size=batch_size, epochs=epochs, verbose=1, validation_split=0.1
+)
+score = model.evaluate(x_test, y_test, batch_size=batch_size, verbose=1)
+print("Test score:", score[0])
+print("Test accuracy:", score[1])
